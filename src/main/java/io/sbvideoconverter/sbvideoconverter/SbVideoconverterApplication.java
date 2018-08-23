9eca58c9c11572ff87d8c397fee5e795b1f3639a
deniojunior <deniofriacamoreirajr@gmail.com>
2018-08-23 11:47:17 -0300

    Configurar a estrutura inicial do projeto
    Foi implementada a estrutura básica do projeto utilizando o framework Spring

---
+++ src/main/java/io/sbvideoconverter/sbvideoconverter/SbVideoconverterApplication.java
@@ -0,0 +1,12 @@
+package io.sbvideoconverter.sbvideoconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbVideoconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbVideoconverterApplication.class, args);
+	}
+}
